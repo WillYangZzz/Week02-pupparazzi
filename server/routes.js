d083b36e41479168a775d936231eb5cd88923235
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-10-18 11:25:00 +1300

    Filter puppy details

--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,15 @@
 import express from 'express'
 const router = express.Router()
 
+import puppyData from './getPuppies.js'
+
 router.get('/:id', (req, res) => {
-  res.send(`puppy ${req.params.id} should be displayed here`)
+  console.log(req.params.id)
+  // console.log(puppyData)
+  const puppy = puppyData.puppies.find((pup) => {
+    return parseInt(pup.id) === parseInt(req.params.id)
+  })
+  res.render('details', puppy)
 })
 
 export default router
