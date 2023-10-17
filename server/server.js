d70ddb9d6e243764eab828a41b1b9d20a87c3a3f
Pathik Modi <modipathik95@gmail.com>
2023-10-18 11:30:25 +1300

    rendering puppies collection on home

--- server/server.js
+++ server/server.js
@@ -1,9 +1,8 @@
 import * as Path from 'node:path'
+import { readdir, readFile, writeFile } from 'node:fs/promises' //promises
 // import * as URL from 'node:url'
-
 import express from 'express'
 import hbs from 'express-handlebars'
-
 
 const server = express()
 
@@ -18,5 +17,17 @@ server.set('view engine', 'hbs')
 server.set('views', Path.resolve('server/views'))
 
 // Your routes/router(s) should go here
+// homepage rendering puppies from home.hbs
+server.get('/', async (req, res) => {
+  try {
+    const filePath = new URL('./data/data.json', import.meta.url)
+    const contents = await readFile(filePath, { encoding: 'utf8' })
+
+    const viewData = JSON.parse(contents)
+    res.render('home', viewData)
+  } catch (err) {
+    console.error(err.message)
+  }
+})
 
 export default server
