0aeb3540c931737531acf322305d22c5de3fcd2c
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-10-18 11:46:06 +1300

    render the edit puppy form

--- server/routes.js
+++ server/routes.js
@@ -3,9 +3,16 @@ const router = express.Router()
 
 import puppyData from './getPuppies.js'
 
+// GET the puppy edit form
+router.get('/edit/:id', (req, res) => {
+  const puppy = puppyData.puppies.find((pup) => {
+    return parseInt(pup.id) === parseInt(req.params.id)
+  })
+  res.render('edit', puppy)
+})
+
+// GET an individual puppy detail
 router.get('/:id', (req, res) => {
-  console.log(req.params.id)
-  // console.log(puppyData)
   const puppy = puppyData.puppies.find((pup) => {
     return parseInt(pup.id) === parseInt(req.params.id)
   })
