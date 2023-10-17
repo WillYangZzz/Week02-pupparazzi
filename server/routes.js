dcd0b8b981cd35231ba9dc4de9dbc8dc52795182
Kirsty <142766386+Kirsty-Ammundsen@users.noreply.github.com>
2023-10-18 11:34:38 +1300

    Go to edit page for the puppies

--- server/routes.js
+++ server/routes.js
@@ -16,4 +16,18 @@ router.get('/:id', async (req, res) => {
   res.render('details', viewData.puppies[id - 1])
 })
 
+// GET /puppies/:id/edit
+router.get('/:id/edit', async (req, res) => {
+  const id = req.params.id
+
+  const puppiesJSON = await readFile(Path.resolve('./server/data/data.json'), {
+    encoding: 'utf8',
+  })
+  const viewData = JSON.parse(puppiesJSON)
+
+  res.render('edit', viewData.puppies[id - 1])
+})
+
+// POST /puppies/:id/edit
+
 export default router
