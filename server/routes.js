455fd19867263238d48f60a7114654a14ca004c7
Kirsty <142766386+Kirsty-Ammundsen@users.noreply.github.com>
2023-10-18 11:23:23 +1300

    Render linked pupparazzi page

--- server/routes.js
+++ server/routes.js
@@ -1,11 +1,19 @@
 import express from 'express'
+import * as Path from 'node:path'
+import { readFile, writeFile } from 'node:fs/promises'
 
 const router = express.Router()
 
 // GET /puppies/
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   const id = req.params.id
-  res.send(`here is ${id} puppy`)
+
+  const puppiesJSON = await readFile(Path.resolve('./server/data/data.json'), {
+    encoding: 'utf8',
+  })
+  const viewData = JSON.parse(puppiesJSON)
+  // console.log(viewData)
+  res.render('details', viewData)
 })
 
 export default router
