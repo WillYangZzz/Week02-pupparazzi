4b26e5cdd3fe6589c1188f559810f9606de82ed3
gapson <hgapson@gmail.com>
2023-10-18 17:48:44 +1300

    adding details rendering page

---
+++ server/routes/details.js
@@ -0,0 +1,16 @@
+// import express from 'express'
+
+// const puppyDetails = express.Router()
+
+// const puppies = puppyDetails.puppies
+// puppyDetails.get('/:id', (req, res) => {
+//   const id = req.params.id
+//   const puppyDetails = puppies.find((puppyName) => {
+//     if (puppyName['id'] == id) {
+//       return true
+//     }
+//   })
+//   res.render('details', puppyDetails)
+// })
+
+// export default puppyDetails
--- server/server.js
+++ server/server.js
@@ -1,9 +1,21 @@
 import * as Path from 'node:path'
-// import * as URL from 'node:url'
-
-import express from 'express'
+import * as URL from 'node:url'
+import express, { Router } from 'express'
 import hbs from 'express-handlebars'
+// import { readdir, readFile } from 'node:fs/promises'
+import fs from 'node:fs/promises'
 
+// server/data
+const __filename = URL.fileURLToPath(import.meta.url)
+const __dirname = Path.dirname(__filename)
+const dataPath = Path.join(__dirname, '/data/data.json')
+const data = await fs.readFile(dataPath, 'utf-8')
+const puppiesData = JSON.parse(data)
+
+console.log(puppiesData)
+
+import puppyRouter from './routes/dog.js'
+// import puppyDetails from './routes/details.js'
 
 const server = express()
 
@@ -19,4 +31,38 @@ server.set('views', Path.resolve('server/views'))
 
 // Your routes/router(s) should go here
 
+server.use('/puppies', puppyRouter)
+// server.use('puppies', puppyDetails.id)
+
+server.get('/', (req, res) => {
+  res.render('home', puppiesData)
+})
+
+const puppiesArray = puppiesData.puppies
+server.get('/:id', (req, res) => {
+  const id = req.params.id
+  const puppyDetails = puppiesArray.find((puppyName) => {
+    if (puppyName['id'] == id) {
+      return true
+    }
+  })
+  res.render('details', puppyDetails)
+})
+
+// server.get('/:id', (req, res) => {
+//   const id = req.params.id
+//   const puppyDetails = puppiesArray.find((puppyName) => {
+//     if (puppyName['id'] == id) {
+//       return true
+//     }
+//   })
+//   res.render('edit', puppyDetails)
+// })
+
+// server.post('/edit/:id', (req, res) => {
+//   const id = req.params.id
+//   const newDetails = req.body
+
+//   console.log(newDetails)
+// })
 export default server
