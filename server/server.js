dece5df31f4ee2c38e7f9d298ec1248661480f98
Vitor <vitormuller11@gmail.com>
2023-10-18 13:39:21 +1300

    adding a routes file

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
 import * as Path from 'node:path'
-// import * as URL from 'node:url'
-
+import * as URL from 'node:url'
 import express from 'express'
 import hbs from 'express-handlebars'
+import {readFile, } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url'
+import { Console } from 'node:console';
+
+
+
+
 
 
 const server = express()
@@ -17,6 +23,22 @@ server.engine('hbs', hbs.engine({ extname: 'hbs' }))
 server.set('view engine', 'hbs')
 server.set('views', Path.resolve('server/views'))
 
+
+        //reading data from the JSON file 
+        const __filename = URL.fileURLToPath(import.meta.url)
+        const __dirname = Path.dirname(__filename)
+        const dataPath = Path.join(__dirname, './data/data.json')
+        let puppydetails = await readFile(dataPath ,{encoding : "utf-8"})
+        console.log(puppydetails);
+        //changing the JSON file into a JS file 
+        let parsePuppy = JSON.parse(puppydetails)
+        console.log(parsePuppy);
+
+
 // Your routes/router(s) should go here
+server.get("/" , (req, res) =>{
+    res.render("home", parsePuppy)  
+})
+
 
 export default server
