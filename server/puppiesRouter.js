05d7cd7ca27079dc6282e4f89e54bff77aa3b36b
Ricky <65267289+ricky-huang-dev@users.noreply.github.com>
2023-10-18 16:46:21 +1300

    implement detailed dog page functionality

---
+++ server/puppiesRouter.js
@@ -0,0 +1,43 @@
+import express from 'express'
+import * as Path from 'node:path'
+// import * as URL from 'node:url'
+import * as URL from 'node:url'
+import { readFile, writeFile } from 'node:fs/promises'
+
+const router = express.Router()
+
+const __filename = URL.fileURLToPath(import.meta.url)
+const __dirname = Path.dirname(__filename)
+const dataFile = Path.join(__dirname, '/data/data.json')
+
+const contents = await readFile(dataFile, { encoding: 'utf8' })
+const parsedData = JSON.parse(contents)
+const puppiesArray = parsedData.puppies
+
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+  const puppy = puppiesArray.find((dog) => {
+    if (dog['id'] == id) {
+      return true
+    }
+  })
+  res.render('details', puppy)
+})
+
+router.get('/edit/:id', (req, res) => {
+  const id = req.params.id
+  const puppy = puppiesArray.find((dog) => {
+    if (dog['id'] == id) {
+      return true
+    }
+  })
+  res.render('edit', puppy)
+})
+
+router.post('/edit/:id', (req, res) => {
+  const id = req.params.id
+  const newData = req.body
+
+  console.log(newData)
+})
+export default router
--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 import * as Path from 'node:path'
 // import * as URL from 'node:url'
+import * as URL from 'node:url'
+import { readFile } from 'node:fs/promises'
 
 import express from 'express'
 import hbs from 'express-handlebars'
+import puppiesRouter from './puppiesRouter.js'
 
+//read data
 
 const server = express()
 
@@ -17,6 +21,19 @@ server.engine('hbs', hbs.engine({ extname: 'hbs' }))
 server.set('view engine', 'hbs')
 server.set('views', Path.resolve('server/views'))
 
+//these lines of code fines the path to the data.json inside data folder
+const __filename = URL.fileURLToPath(import.meta.url)
+const __dirname = Path.dirname(__filename)
+const dataFile = Path.join(__dirname, '/data/data.json')
+
+const contents = await readFile(dataFile, { encoding: 'utf8' })
+const parsedData = JSON.parse(contents)
+
 // Your routes/router(s) should go here
+server.get('/', (req, res) => {
+  res.render('home', parsedData)
+})
+
+server.use('/', puppiesRouter)
 
 export default server
