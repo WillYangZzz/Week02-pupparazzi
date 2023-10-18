b2d1070c5c100cb613af6ab6c02434fe8105088e
Samir Lekinwala <samirlekinwala@gmail.com>
2023-10-18 22:32:29 +1300

    Clean up some of the console logs

--- server/routes.js
+++ server/routes.js
@@ -1,14 +1,11 @@
 import express from 'express'
 import { puppyData } from './server.js'
 import { writeFile } from 'fs/promises'
-// import readFile from 'node'
 const router = express.Router()
 const dogData = puppyData()
 
 router.get('/:id', async (req, res) => {
-  puppyData()
   let data = await dogData
-  // console.log(data)
   const id = req.params.id
   const value = data.puppies.find((item) => {
     if (item.id == id) {
@@ -17,6 +14,7 @@ router.get('/:id', async (req, res) => {
   })
   res.render('details', value)
 })
+
 router.get('/edit/:id', async (req, res) => {
   let data = await dogData
   const id = req.params.id
@@ -39,16 +37,13 @@ router.post('/edit/:id', async (req, res) => {
     breed: req.body.breed,
     owner: req.body.owner,
   }
-  // console.log(puppyEdit)
   let data = await dogData
-  // data.puppies.breed = 'bndsjkahbd'
   const value = data.puppies.find((item) => {
     if (item.id == id) {
       puppyEdit.image = item.image
       let filtered = data.puppies.filter((x) => x.id != item.id)
       filtered.push(puppyEdit)
       newPuppyObject.puppies = filtered
-      console.log('new object array: ', newPuppyObject.puppies)
       const jsonPuppies = JSON.stringify(newPuppyObject)
       writeFile('server/data/data.json', jsonPuppies, 'utf-8')
       return true
