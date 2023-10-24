e62332ffb2829fa1b54e79ba51f0b76539ee115a
gapson <hgapson@gmail.com>
2023-10-24 23:17:42 +1300

    adding edit route

--- server/server.js
+++ server/server.js
@@ -1,21 +1,15 @@
 import * as Path from 'node:path'
-import * as URL from 'node:url'
-import express, { Router } from 'express'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = Path.dirname(__filename)
+
+import express from 'express'
 import hbs from 'express-handlebars'
-// import { readdir, readFile } from 'node:fs/promises'
+import puppyRouter from './routes.js'
 import fs from 'node:fs/promises'
 
-// server/data
-const __filename = URL.fileURLToPath(import.meta.url)
-const __dirname = Path.dirname(__filename)
-const dataPath = Path.join(__dirname, '/data/data.json')
-const data = await fs.readFile(dataPath, 'utf-8')
-const puppiesData = JSON.parse(data)
-
-console.log(puppiesData)
-
-import puppyRouter from './routes/dog.js'
-// import puppyDetails from './routes/details.js'
+const dataPath = Path.join(__dirname, './data/data.json')
 
 const server = express()
 
@@ -32,37 +26,11 @@ server.set('views', Path.resolve('server/views'))
 // Your routes/router(s) should go here
 
 server.use('/puppies', puppyRouter)
-// server.use('puppies', puppyDetails.id)
 
-server.get('/', (req, res) => {
+server.get('/', async (req, res) => {
+  const data = await fs.readFile(dataPath, 'utf-8')
+  const puppiesData = JSON.parse(data)
   res.render('home', puppiesData)
 })
 
-const puppiesArray = puppiesData.puppies
-server.get('/:id', (req, res) => {
-  const id = req.params.id
-  const puppyDetails = puppiesArray.find((puppyName) => {
-    if (puppyName['id'] == id) {
-      return true
-    }
-  })
-  res.render('details', puppyDetails)
-})
-
-// server.get('/:id', (req, res) => {
-//   const id = req.params.id
-//   const puppyDetails = puppiesArray.find((puppyName) => {
-//     if (puppyName['id'] == id) {
-//       return true
-//     }
-//   })
-//   res.render('edit', puppyDetails)
-// })
-
-// server.post('/edit/:id', (req, res) => {
-//   const id = req.params.id
-//   const newDetails = req.body
-
-//   console.log(newDetails)
-// })
 export default server
