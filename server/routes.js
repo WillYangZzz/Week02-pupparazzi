530ec620d9ab6de814ee112f3e280331a88ae2c6
Vitor <142807308+vitor-fernandess@users.noreply.github.com>
2023-10-30 16:02:16 +1300

    puppys done

--- server/routes.js
+++ server/routes.js
@@ -1,53 +1,69 @@
-import express from "express"
+import express from 'express'
 import * as Path from 'node:path'
 import * as URL from 'node:url'
-import { readFile } from "node:fs/promises"
+import { readFile } from 'node:fs/promises'
+import { log } from 'node:console'
+import { writeFile } from 'node:fs/promises'
+import { readingData } from './server.js'
 
 const router = express.Router()
 
-
 //reading data from the JSON file
-const __filename = URL.fileURLToPath(import.meta.url)
-const __dirname = Path.dirname(__filename)
-const dataPath = Path.join(__dirname, './data/data.json')
-let puppydetails = await readFile(dataPath ,{encoding : "utf-8"})
-//changing the JSON file into a JS object file 
-let parsePuppy = JSON.parse(puppydetails)
-
+// const __filename = URL.fileURLToPath(import.meta.url)
+// const __dirname = Path.dirname(__filename)
+// const dataPath = Path.join(__dirname, './data/data.json')
+// let puppydetails = await readFile(dataPath, { encoding: 'utf-8' })
+// //changing the JSON file into a JS object file
 
 // returning the array ID and matching to the clicked puppy ID
-router.get("/:id", (req, res) =>{
-    const value = req.params.id
-    let dog = parsePuppy["puppies"].find((dog)=>{
-        if(dog["id"] === Number(value)){
-            return true
-        }
-    })
+router.get('/:id', async (req, res) => {
+  const value = req.params.id
+  let parsePuppy = await readingData()
 
-    res.render("details", dog )
-    
+  let dog = parsePuppy['puppies'].find((dog) => {
+    if (dog['id'] === Number(value)) {
+      return true
+    }
+  })
+
+  res.render('details', dog)
 })
 
-router.get("/edit/:id", (req, res) =>{
-    const value = req.params.id
-    let dog = parsePuppy['puppies'].find((dog)=>{
-        if(dog["id"] === Number(value)){
-            return true
-        }
-    })
-        res.render("edit", dog)
-    })
-
-router.post("/edit/:id", (req, res) =>{
-const value = req.params.id
-// create form puppy using values from req.body
-
-// find the right puppy from data
-
-// re-assign the right puppy's values to be form puppy's values
-
-// redirect to single puppy page so user can see their changes. 
+router.get('/edit/:id', async (req, res) => {
+  const value = req.params.id
+  const parsePuppy = await readingData()
+  let dog = parsePuppy['puppies'].find((dog) => {
+    if (dog['id'] === Number(value)) {
+      return true
+    }
+  })
+  res.render('edit', dog)
 })
 
+router.post('/edit/:id', async (req, res) => {
+  const value = req.params.id
+  const parsePuppy = await readingData()
+  // create form puppy using values from req.body
 
+  const dogObj = {
+    id: Number(value),
+    name: req.body.name,
+    breed: req.body.breed,
+    owner: req.body.owner,
+    image: req.body.image,
+  }
+  // find the right puppy from data
+  const findingPuppy = parsePuppy['puppies'].map((dog) => {
+    if (dog['id'] === Number(value)) {
+      return (dog = dogObj)
+    } else {
+      return dog
+    }
+  })
+  const puppiesToJson = JSON.stringify({ puppies: findingPuppy }, null, 2)
+  await writeFile('server/data/data.json', puppiesToJson, 'utf-8')
+  // re-assign the right puppy's values to be form puppy's values
+  res.redirect(`/puppies/${value}`)
+  // redirect to single puppy page so user can see their changes.
+})
 export default router
--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ import * as Path from 'node:path'
 import * as URL from 'node:url'
 import express from 'express'
 import hbs from 'express-handlebars'
-import {readFile, } from 'node:fs/promises';
-import router from './routes.js';
+import { readFile } from 'node:fs/promises'
+import router from './routes.js'
 
 const server = express()
 
@@ -17,26 +17,25 @@ server.engine('hbs', hbs.engine({ extname: 'hbs' }))
 server.set('view engine', 'hbs')
 server.set('views', Path.resolve('server/views'))
 
+//reading data from the JSON file
+const __filename = URL.fileURLToPath(import.meta.url)
+const __dirname = Path.dirname(__filename)
+const dataPath = Path.join(__dirname, './data/data.json')
 
-
-        //reading data from the JSON file 
-        const __filename = URL.fileURLToPath(import.meta.url)
-        const __dirname = Path.dirname(__filename)
-        const dataPath = Path.join(__dirname, './data/data.json')
-        let puppydetails = await readFile(dataPath ,{encoding : "utf-8"})
-        console.log(puppydetails);
-        //changing the JSON file into a JS object file 
-        let parsePuppy = JSON.parse(puppydetails)
-        
-
+//Making it a function
+export async function readingData() {
+  let puppydetails = await readFile(dataPath)
+  let parsePuppy = JSON.parse(puppydetails)
+  return parsePuppy
+}
 // using created routes
-server.use("/puppies", router)
+server.use('/puppies', router)
 // server.use('/edit'  , router)
 
 // Your routes/router(s) should go here
-server.get("/" , (req, res) =>{
-    res.render("home", parsePuppy)  
+server.get('/', async (req, res) => {
+  const parsePuppy = await readingData()
+  res.render('home', parsePuppy)
 })
-
 
 export default server
